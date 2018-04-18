51c3659771d2cfa82bceaea2fcce3bffc1a05601
Kevin Mikami <mikamikm@gmail.com>
2018-04-18 21:30:30 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -2,11 +2,11 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 
 def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "   |   |   "
 puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "   |   |   "
 puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
+puts "   |   |   "
 
   puts "Where would you like to go?"
 
